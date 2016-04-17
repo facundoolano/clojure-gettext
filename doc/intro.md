e06c15d5bcce8d34315ad521d6ae6bb5cf2b79f4
Facundo Olano <facundo.olano@gmail.com>
2016-04-17 19:49:23 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gettext
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gettext "0.1.0"
+  :description "Utilities to write multilingual clojure programs"
+  :url "https://github.com/facundoolano/clojure-gettext"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
