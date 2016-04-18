eeded60f0e3e1eb1220d5a732660c7230a125ecf
Facundo Olano <facundo.olano@gmail.com>
2016-04-17 22:48:23 -0300

    added test cases

--- test/gettext/core_test.clj
+++ test/gettext/core_test.clj
@@ -2,17 +2,29 @@
   (:require [clojure.test :refer :all]
             [gettext.core :refer :all]))
 
+(def dictionary {"Hello, world!" "Hola, mundo!"
+                 "Hello, %s" "Hola, %s"
+                 "the %s is closed." #(if (= :female (:gender %))
+                                         "la %s esta cerrada"
+                                         "el %s esta cerrado")})
+
 (deftest gettext-test
-  (testing "Literal replacement")
-  (testing "Format replacement")
-  (testing "Ignore format argument")
-  (testing "Default to key when missing")
-  (testing "Use empty context when replacement is a function"))
+  (with-bindings {#'gettext.core/*text-source* dictionary}
+    (testing "Literal replacement"
+      (is (= "Hola, mundo!" (_ "Hello, world!"))))
+    (testing "Format replacement"
+      (is (= "Hola, Facundo" (_ "Hello, %s" "Facundo"))))
+    (testing "Ignore format argument"
+      (is (= "Hola, mundo!" (_ "Hello, world!" "unused" "argument"))))
+    (testing "Default to key when missing"
+      (is (= "Goodbye cruel world..." (_ "Goodbye cruel world..."))))
+    (testing "Use empty context when replacement is a function"
+      (is (= "el auto esta cerrado" (_ "the %s is closed." "auto"))))))
 
 (deftest pgettext-test
-  (testing "Change replacement based on context")
-  (testing "Ignore context when replacement is not a function"))
-
-; remove this in case already used above
-(deftest source-test
-  (testing "change *text-source* binding."))
+  (with-bindings {#'gettext.core/*text-source* dictionary}
+    (testing "Change replacement based on context"
+      (is (= "el auto esta cerrado" (p_ {:gender :male} "the %s is closed." "auto")))
+      (is (= "la puerta esta cerrada" (p_ {:gender :female} "the %s is closed." "puerta"))))
+    (testing "Ignore context when replacement is not a function"
+      (is (= "Hola, Facundo" (p_ {:gender :male} "Hello, %s" "Facundo"))))))
